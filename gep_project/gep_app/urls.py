73592635866787e7580cfadc38a880c58263ab08
devika <devika@gmail.com>
2018-07-26 22:48:20 +0530

    Login page UI done, change password UI done, Stage 1 UI incomplete

--- gep_project/gep_app/urls.py
+++ gep_project/gep_app/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
 	path('login',views.login,name='login'),
-	path('password_reset',views.password_reset,name='password_reset'),
+	path('change_password',views.change_password,name='change_password'),
 	path('stage_1',views.stage_1,name='stage_1'),
 ]
--- gep_project/gep_app/views.py
+++ gep_project/gep_app/views.py
@@ -4,10 +4,10 @@ def login(request):
 	context = {}
 	return render(request, 'login.html', context)
 
-def password_reset(request):
+def change_password(request):
 	context = {}
-	return render(request, 'password_reset.html', context)
+	return render(request, 'change_password.html', context)
 
 def stage_1(request):
 	context = {}
-	return render(request, 'stage1.html', context)
+	return render(request, 'stage_1.html', context)
