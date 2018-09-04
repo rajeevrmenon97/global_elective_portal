1fd003ed3a72776b9920bfc330ecf0f46ca80c3d
devika <devika@gmail.com>
2018-09-04 22:47:47 +0530

    before changing formset to json field

--- web/gep_project/gep_app/forms.py
+++ web/gep_project/gep_app/forms.py
@@ -2,68 +2,59 @@ from django import forms
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext as _
 from django.db.models import Q
-from .models import Student, Course
+from .models import Student, Course, Elective, Elective_Seats, Faculty
 from datetime import datetime
 
 class StudentAcademicsForm(forms.ModelForm):
-    class Meta:
-        model = Student
-        fields = ['name','user','current_CGPA','next_semester','required_elective_count','core_slots','past_courses','submission_datetime']
+	class Meta:
+		model = Student
+		fields = ['current_CGPA','next_semester','required_elective_count','core_slots','past_courses','submission_datetime']
 
-    def __init__(self, *args, **kwargs):
-        super(StudentAcademicsForm, self).__init__(*args, **kwargs)
-        self.fields['user'].widget = forms.TextInput(attrs={
-                'readonly': True,
-                'class': 'form-control',
-            })
-        self.fields['name'].widget.attrs.update({
-                'readonly': True,
-                'class': 'form-control',
-           }) 
-        self.fields['current_CGPA'].widget.attrs.update({
-                'placeholder': 'CGPA',
-                'class': 'form-control',
-            })        
-        self.fields['next_semester'].widget.attrs.update({
-                'placeholder': 'Semester',
-                'class': 'form-control',
-            })
-        self.fields['required_elective_count'].widget.attrs.update({
-                'placeholder': 'Number of global electives to be taken',
-                'class': 'form-control',
-            })
-        self.fields['past_courses'].widget.queryset = Course.objects.all().order_by('name')
-        self.fields['past_courses'].widget.attrs.update({
-                'placeholder': 'No courses choosen',
-                'class': 'form-control',
-            })
-        self.fields['core_slots'].widget = forms.SelectMultiple(attrs={
-                'placeholder': 'No slots chosen',
-                'class': 'form-control',
-            },choices=self.Meta.model.SLOT_CHOICES)
+	def __init__(self, *args, **kwargs):
+		super(StudentAcademicsForm, self).__init__(*args, **kwargs)
+		self.fields['current_CGPA'].widget.attrs.update({
+				'placeholder': 'CGPA',
+				'class': 'form-control',
+			})
+		self.fields['next_semester'].widget.attrs.update({
+				'placeholder': 'Semester',
+				'class': 'form-control',
+			})
+		self.fields['required_elective_count'].widget.attrs.update({
+				'placeholder': 'Number of global electives to be taken',
+				'class': 'form-control',
+			})
+		self.fields['past_courses'].widget.queryset = Course.objects.all().order_by('name')
+		self.fields['past_courses'].widget.attrs.update({
+				'placeholder': 'No courses choosen',
+				'class': 'form-control',
+			})
+		self.fields['core_slots'].widget = forms.SelectMultiple(attrs={
+				'class': 'form-control',
+			},choices=self.Meta.model.SLOT_CHOICES)
  
-    def clean_current_CGPA(self):
-        current_CGPA = self.cleaned_data.get('current_CGPA')
-        if current_CGPA is None:
-            raise ValidationError(_('This field is required'), code='empty')
-        return current_CGPA
+	def clean_current_CGPA(self):
+		current_CGPA = self.cleaned_data.get('current_CGPA')
+		if current_CGPA is None:
+			raise ValidationError(_('This field is required'), code='empty')
+		return current_CGPA
 
-    def clean_next_semester(self):
-        next_semester = self.cleaned_data.get('next_semester')
-        if next_semester is None:
-            raise ValidationError(_('This field is required'), code='empty')
-        return next_semester
+	def clean_next_semester(self):
+		next_semester = self.cleaned_data.get('next_semester')
+		if next_semester is None:
+			raise ValidationError(_('This field is required'), code='empty')
+		return next_semester
 
-    def clean_required_elective_count(self):
-        required_elective_count = self.cleaned_data.get('required_elective_count')
-        if required_elective_count is None:
-            raise ValidationError(_('This field is required'), code='empty')
-        return required_elective_count
+	def clean_required_elective_count(self):
+		required_elective_count = self.cleaned_data.get('required_elective_count')
+		if required_elective_count is None:
+			raise ValidationError(_('This field is required'), code='empty')
+		return required_elective_count
 
-    def clean_submission_datetime(self):
-        submission_datetime = datetime.now()
-        return submission_datetime
-
+	def clean_submission_datetime(self):
+		submission_datetime = datetime.now()
+		return submission_datetime
+	
 #    def clean_password(self):
 #        cleaned_data = super().clean()
 #        password = cleaned_data.get('password')
@@ -81,3 +72,149 @@ class StudentAcademicsForm(forms.ModelForm):
 #            self.add_error('password',error_msg)
 
 #        return cleaned_data
+
+class CourseCreationForm(forms.ModelForm):	
+	class Meta:
+		model = Course
+		fields = ['course_id','name','credits','pre_requisites','cot_requisite','cgpa_cutoff','mode_of_allotment']
+
+	def __init__(self, *args, **kwargs):
+		super(CourseCreationForm, self).__init__(*args, **kwargs)
+		
+		self.fields['course_id'].label = 'Course ID'
+		self.fields['course_id'].widget.attrs.update({
+				'placeholder': 'Course ID',
+				'class': 'form-control',
+           })
+		self.fields['name'].label = 'Course Name'
+		self.fields['name'].widget.attrs.update({
+				'placeholder': 'Course Name',
+				'class': 'form-control',
+			})
+		self.fields['credits'].label = 'Number of credits'
+		self.fields['credits'].widget.attrs.update({
+				'placeholder': 'Credits',
+				'class': 'form-control',
+			})
+		self.fields['pre_requisites'].label = 'Pre requisites (If any)'
+		self.fields['pre_requisites'].widget.attrs.update({
+				'placeholder': 'Enter the pre requisites...',
+				'class': 'form-control',
+				'rows':'4',
+			})
+		self.fields['cot_requisite'].label = 'Consent Of Teacher required or not'
+		self.fields['cot_requisite'].widget.attrs.update({
+				'class': 'checkbox-big',
+			})
+		self.fields['cgpa_cutoff'].label = 'CGPA cutoff (If any)'
+		self.fields['cgpa_cutoff'].widget.attrs.update({
+				'placeholder': 'CGPA cutoff',
+				'class': 'form-control',
+			})
+		self.fields['mode_of_allotment'].label = 'Mode of allotment'
+		self.fields['mode_of_allotment'].widget.attrs.update({
+				'class': 'form-control',
+			})
+		
+	def save(self, dept, commit=True):
+		self.instance.dept = dept
+		return super().save()
+		
+
+class AddElectiveForm(forms.Form):
+	elective_data = forms.CharField(max_length=5000, attrs={
+				'class': 'form-control',
+				'required': True,
+			})
+	
+	def clean_elective_data(self):
+		elective_data = self.cleaned_data.get('elective_data')
+		try:
+			json_data = json.loads(elective_data) 
+		except:
+			raise forms.ValidationError("Invalid data")
+
+		for slot_data in json_data:
+			slot = slot_data['slot']
+			faculty = slot_data['faculty']
+			print(slot)
+			print(faculty)
+			for dept, max_seat in max_seats.items():
+				print(dept)
+				print(max_seat)
+		return elective_data
+		
+#	def save(self, course, commit=True):
+#		self.instance.course = course
+#		return super().save()
+
+		
+
+class ElectiveCreationForm(forms.ModelForm):	
+	class Meta:
+		model = Elective
+		fields = ['slot','faculty']
+
+	def __init__(self, *args, **kwargs):
+		super(ElectiveCreationForm, self).__init__(*args, **kwargs)
+		
+		self.fields['slot'].widget.attrs.update({
+				'class': 'form-control',
+           })
+		self.fields['faculty'].queryset = Faculty.objects.all().order_by('name')
+		self.fields['faculty'].widget.attrs.update({
+				'class': 'form-control',
+			})
+		
+	def save(self, course, commit=True):
+		self.instance.course = course
+		return super().save()
+
+
+class ElectiveSeatsCreationForm(forms.ModelForm):	
+	class Meta:
+		model = Elective_Seats
+		fields = ['dept','max_seats']
+
+	def __init__(self, *args, **kwargs):
+		super(ElectiveSeatsCreationForm, self).__init__(*args, **kwargs)
+		
+		self.fields['dept'].widget.attrs.update({
+				'class': 'form-control',
+           })
+		self.fields['max_seats'].widget.attrs.update({
+				'class': 'form-control',
+			})
+		
+	def save(self, elective, commit=True):
+		self.instance.elective = elective
+		return super().save()
+	
+#class AddElectiveForm(forms.Form):
+#	slots = forms.SelectMultiple(attrs={
+#				'class': 'form-control',
+#				'required': True,
+#			},choices=Elective.SLOT_CHOICES)
+#	max_seats = 
+	
+	
+#class ElectiveForm(MultiModelForm):
+#	form_classes = {
+#        'course': CourseCreationForm,
+#        'elective': ElectiveCreationForm,
+#        'elective_seat': ElectiveSeatForm,
+#    }
+#	
+#		objects = super(ElectiveForm, self).save(commit=False)
+#
+#		if commit:
+#			course = objects['course']
+#			user.save()
+#			for elective in objects['electives']:
+#				elective.course = course
+#				elective.save()
+#				for elective_seat in objects['elective_seats']:
+#					elective_seat.elective = elective
+#					elective_seat.max_seats = elective_seat.max_seats / objects['electives'].length()
+#					elective_seat.save()
+#		return objects
--- web/gep_project/gep_app/views.py
+++ web/gep_project/gep_app/views.py
@@ -5,8 +5,9 @@ from django.contrib.auth import authenticate, login as auth_login , logout as au
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.db.models import Q
 from .models import Student, Elective, Elective_Preference, COT_Allotment
-from .forms import StudentAcademicsForm
+from .forms import StudentAcademicsForm, CourseCreationForm, ElectiveCreationForm, ElectiveSeatsCreationForm
 from .allotment import *
+from django.forms import formset_factory
 
 def login(request):
 	if request.method == 'POST':
@@ -60,27 +61,11 @@ def student_home(request):
 		form = StudentAcademicsForm(instance=student)
 		
 	context = {
-		'student': student,
+		'user':user.username,
+		'student_name': student.name,
 		'form':form,
 	}
 	return render(request, 'student/home.html', context)
-
-def get_eligible_electives(student):
-	""" Return all electives eligible for which the student is eligible ordered by slot and department"""
-	#All electives ordered by slot and department
-	electives = Elective.objects.all().order_by('slot', 'course__dept__name')
-	#Filter global electives
-	global_electives = electives.filter(~Q(course__dept=student.dept))
-	#Filter out electives that were already taken
-	print(student.past_courses.all())
-	not_taken_electives = global_electives.filter(~Q(course__in=student.past_courses.all()))
-	#Filter out electives offered in slots of core courses
-	slot_satisfied_electives = not_taken_electives.filter(~Q(slot__in=student.core_slots))
-	#Filter electives with CGPA requirement satisfied
-	cgpa_satisfied_electives = slot_satisfied_electives.filter(course__cgpa_cutoff__lte=student.current_CGPA)
-	#Filter electives with COT requirement satisfied
-	cot_satisfied_electives = [elective for elective in cgpa_satisfied_electives if not elective.course.cot_requisite or COT_Allotment.objects.filter(student=student,elective=elective).exists()]
-	return cot_satisfied_electives
 
 @login_required
 @user_passes_test(student_check)
@@ -94,14 +79,33 @@ def preference_list_check(student,elective_id=None):
 	else:
 		return Elective_Preference.objects.filter(student=student,elective_id=elective_id).exists()
 
+def get_eligible_electives(student):
+	""" Return all electives eligible for which the student is eligible ordered by slot and department"""
+	#1) All electives ordered by slot and department
+	electives = Elective.objects.all().order_by('slot', 'course__dept__name')
+	#2) Filter global electives
+	global_electives = electives.filter(~Q(course__dept=student.dept))
+	#3) Filter out electives that were already taken
+	not_taken_electives = global_electives.filter(~Q(course__in=student.past_courses.all()))
+	#4) Filter out electives offered in slots of core courses
+	slot_satisfied_electives = not_taken_electives.filter(~Q(slot__in=student.core_slots))
+	#5) Filter electives with CGPA requirement satisfied
+	cgpa_satisfied_electives = slot_satisfied_electives.filter(course__cgpa_cutoff__lte=student.current_CGPA)
+	#6) Filter electives with COT requirement satisfied
+	cot_satisfied_electives = [elective for elective in cgpa_satisfied_electives if not elective.course.cot_requisite or COT_Allotment.objects.filter(student=student,elective=elective).exists()]
+	return cot_satisfied_electives
+
 def get_preference_list(student):
-	"""Returns elective preference list submitted by the student"""
+	"""Returns elective preference list submitted by the student along with new electives and after removing removed electives"""
+	#1) All electives submitted by the student
 	elective_ids = Elective_Preference.objects.filter(student=student).order_by('priority_rank').values_list('elective',flat=True)
-	electives = Elective.objects.filter(pk__in=elective_ids)
-	newly_added_electives = list(set(get_eligible_electives(student)) - set(electives))
-	removed_electives = list(set(electives) - set(get_eligible_electives(student)))
-	list(electives).extend(newly_added_electives)
-	return [elective for elective in electives if elective not in removed_electives]
+	electives = list(Elective.objects.filter(pk__in=elective_ids))
+	#2) Add recently added electives in the end
+	recently_added_electives = list(set(get_eligible_electives(student)) - set(electives))
+	electives.extend(recently_added_electives)
+	#3) Remove recently removed electives
+	recently_removed_electives = list(set(electives) - set(get_eligible_electives(student)))
+	return [elective for elective in electives if elective not in recently_removed_electives]
 
 @login_required
 @user_passes_test(student_check)
@@ -125,7 +129,7 @@ def preference_submission(request):
 			else:
 				allotment = Elective_Preference.objects.create(student=student, elective_id=elective_id, priority_rank=value)
 				
-	electives = get_preference_list(student) if preference_list_check(student) else get_eligible_electives(student)
+	electives = get_preference_list(student)
 	context = {
 		'electives':electives,
 	}
@@ -139,6 +143,35 @@ def faculty_home(request):
 @login_required
 @user_passes_test(department_check)
 def department_home(request):
+	user = request.user
+	user_dept = Department.objects.get(user=user)
+	depts = Department.objects.all()
+	initial_data = []
+	for dept in depts:
+		initial_data.append({
+			'dept':dept
+		})
+	ElectiveCreationFormSet = formset_factory(ElectiveCreationForm)
+	ElectiveSeatsCreationFormSet = formset_factory(ElectiveSeatsCreationForm)
+	
+
+	if request.method == 'POST':
+		print(request.POST)
+		course_form = CourseCreationForm(request.POST)
+		if course_form.is_valid():
+			course_form.save(user_dept)
+	else:
+		course_form = CourseCreationForm()
+		elective_formset = ElectiveCreationFormSet()
+		elective_seats_formset = ElectiveSeatsCreationFormSet(initial=initial_data)
+
+	context = {
+		'user_dept':user_dept,
+		'depts':depts,
+		'form':course_form,
+		'elective_formset':elective_formset,
+		'elective_seats_formset':elective_seats_formset,
+	}
 	return render(request, 'department/home.html', context)
 	
 def change_password(request):
